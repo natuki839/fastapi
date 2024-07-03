edcaf018080921f780b50e1c33fdce410c053937
natuki <k_07219_15@outlook.jp>
2024-07-04 05:20:05 +0900

    index.html

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount(path="/static", app=StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def read_root():
-    return {"msg" : "HelloWorld"}
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    context = {"request" : request}
+    return templates.TemplateResponse("index.html", context)
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
